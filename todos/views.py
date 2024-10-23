5d4e3f19dd852db7e254328e0871225fbd731479
akhila <akhilavarghese174@gmail.com>
2024-10-23 12:12:40 +0530

    Added patch api to update a task.

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,5 @@
+from pydoc import describe
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import TaskSerializer
@@ -16,3 +18,17 @@ class TaskAPIView(APIView):
                 return Response({'message': 'Task added successfully', 'data': request.data},status=status.HTTP_200_OK)  # Created status code
         except Exception as e:
             return Response({'message': 'Error in adding task','data':{}}, status=status.HTTP_404_NOT_FOUND)
+
+    def patch(self, request):
+        task_id = request.data.get('task_id')
+        task_info = Task.objects.filter(task_id=task_id).first()
+        title = request.data.get('title')
+        description = request.data.get('description')
+        is_done = request.data.get('is_done')
+
+        data = {'title': title, 'description': description, 'is_done': is_done}
+        task_serializer = TaskSerializer(instance=task_info, data=data, partial=True)
+        if task_serializer.is_valid():
+            task_serializer.update(task_info, validated_data = data)
+            return Response({'message': 'task updated successfully', 'data':request.data}, status=status.HTTP_200_OK)
+        return Response({'message': 'Error in adding task', 'data': {}}, status=status.HTTP_404_NOT_FOUND)
